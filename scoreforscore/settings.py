72337116e994c8e1ac68587e043321e118f78c68
brinaseidel <brina.seidel@gmail.com>
2018-09-03 13:42:56 -0400

    Altering static file management

--- scoreforscore/settings.py
+++ scoreforscore/settings.py
@@ -137,7 +137,7 @@ DATABASES['default'].update(db_from_env)
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Allowed hosts
 ALLOWED_HOSTS = ['scoreforscore.herokuapp.com', '127.0.0.1']
